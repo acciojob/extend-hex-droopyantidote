0ab3f54275cd2f9df0698a44583256fbdd4ce327
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2024-02-10 19:05:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const extendHex = (shortHex) => {
   // write your code here
+	// Remove '#' if it exists
+    shortHex = shortHex.replace('#', '');
+
+    // Check if the shortHex has only 3 characters
+    if (shortHex.length === 3) {
+        // Extend each color component
+        let fullHex = '';
+        for (let i = 0; i < 3; i++) {
+            // Get the color component
+            let component = shortHex[i];
+            // Extend the color component by repeating it
+            fullHex += component + component;
+        }
+        // Add '#' prefix and return the full hex code
+        return '#' + fullHex;
+    } else {
+        // If the input is not a short hex code, return null or throw an error
+        return null; // Or you can throw an error here
+    }
 };
 
 // Do not change the code below.
